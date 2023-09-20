d19e32d47ad0cf275912bfc521037324bff79313
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-09-20 09:04:55 +0200

    Add files via upload

---
+++ uvodnaVjezba02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaVjezba02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 7;
+            double c = 12.5, d = 16.7;
+            double x;
+
+            x = (a + b + c + d)/4;
+            Console.WriteLine("Prosječna vrijednost brojeva 10, 7, 12.5, 16.7 je " + x);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
